96edc539a281d50cc5c8a546161166c3b1ac8a3a
SeungHyeob Han <o0tmdguq0o@gmail.com>
2024-08-18 21:53:28 +0900

    [Add] [Camera] 이미지 촬영 가이드라인 추가, 다크모드 해제

--- app/src/main/java/com/glion/skinscanner_and/SkinScannerApplication.kt
+++ app/src/main/java/com/glion/skinscanner_and/SkinScannerApplication.kt
@@ -1,10 +1,12 @@
 package com.glion.skinscanner_and
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class SkinScannerApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
